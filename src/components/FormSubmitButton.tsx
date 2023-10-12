74c61cff5f39afb2aeaa2de7308c9d0dc8c6cdb2
krzysztoffila <umowy.fila@gmail.com>
2023-10-12 09:37:25 +0200

    Add loading animation on form button

--- src/components/FormSubmitButton.tsx
+++ src/components/FormSubmitButton.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { ComponentProps } from "react";
-
+import { experimental_useFormStatus as useFormStatus } from "react-dom";
 type FormSubmitButtonProps = {
   children: React.ReactNode;
   className?: string;
@@ -9,9 +9,17 @@ type FormSubmitButtonProps = {
 export default function FormSubmitButton({
   children,
   className,
+  ...props
 }: FormSubmitButtonProps) {
+  const { pending } = useFormStatus();
   return (
-    <button className={`btn-primary btn ${className}`} type="submit">
+    <button
+      {...props}
+      className={`btn-primary btn ${className}`}
+      type="submit"
+      disabled={pending}
+    >
+      {pending && <span className="loading loading-spinner" />}
       {children}
     </button>
   );
