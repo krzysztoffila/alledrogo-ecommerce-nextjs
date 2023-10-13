99fabe3096312912d22b40c00db372128dcaac20
krzysztoffila <umowy.fila@gmail.com>
2023-10-13 19:57:32 +0200

    add formating

--- src/components/FormSubmitButton.tsx
+++ src/components/FormSubmitButton.tsx
@@ -2,25 +2,25 @@
 import { ComponentProps } from "react";
 import { experimental_useFormStatus as useFormStatus } from "react-dom";
 type FormSubmitButtonProps = {
-  children: React.ReactNode;
-  className?: string;
+    children: React.ReactNode;
+    className?: string;
 } & ComponentProps<"button">;
 
 export default function FormSubmitButton({
-  children,
-  className,
-  ...props
+    children,
+    className,
+    ...props
 }: FormSubmitButtonProps) {
-  const { pending } = useFormStatus();
-  return (
-    <button
-      {...props}
-      className={`btn-primary btn ${className}`}
-      type="submit"
-      disabled={pending}
-    >
-      {pending && <span className="loading loading-spinner" />}
-      {children}
-    </button>
-  );
+    const { pending } = useFormStatus();
+    return (
+        <button
+            {...props}
+            className={`btn-primary btn ${className}`}
+            type="submit"
+            disabled={pending}
+        >
+            {pending && <span className="loading loading-spinner" />}
+            {children}
+        </button>
+    );
 }
