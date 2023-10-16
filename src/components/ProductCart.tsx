e29a3b2dc40623bd48123ad85a9f639588e8838b
krzysztoffila <umowy.fila@gmail.com>
2023-10-16 20:51:10 +0200

    Add product image

--- src/components/ProductCart.tsx
+++ src/components/ProductCart.tsx
@@ -1,6 +1,7 @@
 import { Product } from "@prisma/client";
 import Link from "next/link";
 import PriceTag from "@/components/PriceTag";
+import Image from "next/image";
 
 interface ProductCartProps {
   product: Product;
@@ -12,6 +13,15 @@ export default function ProductCart({ product }: ProductCartProps) {
       href={"/products/" + product.id}
       className="card w-full bg-base-100 transition-shadow hover:shadow-xl"
     >
+      <figure>
+        <Image
+          src={product.imageUrl}
+          alt={product.name}
+          width={800}
+          height={400}
+          className="h-48 object-cover"
+        />
+      </figure>
       <div className="card-body">
         <h2 className="card-title">{product.name}</h2>
         <p>{product.description}</p>
