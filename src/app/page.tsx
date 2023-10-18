afe2b1a569611a8c0365ddb8c1947da83cee2d12
krzysztoffila <umowy.fila@gmail.com>
2023-10-18 20:36:19 +0200

    add hero

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
-import ProductCart from "@/components/ProductCart";
 import { prisma } from "@/lib/db/prisma";
+import Image from "next/image";
 
 export default async function Home() {
   const products = await prisma.product.findMany({
@@ -10,7 +10,16 @@ export default async function Home() {
 
   return (
     <div>
-      <ProductCart product={products[0]} />
+      <div className="hero rounded-xl bg-base-200">
+        <Image
+          src={products[0].imageUrl}
+          alt={products[0].name}
+          width={400}
+          height={800}
+          className="w-full max-w-sm rounded-lg shadow-2xl"
+          priority
+        />
+      </div>
     </div>
   );
 }
