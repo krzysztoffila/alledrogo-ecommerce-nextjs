865619b6ff7d90a978379caeb789ee62c2dd1ae5
krzysztoffila <umowy.fila@gmail.com>
2023-10-25 14:17:52 +0200

    Add async funtions

--- src/app/products/[id]/page.tsx
+++ src/app/products/[id]/page.tsx
@@ -1,21 +1,41 @@
 import PriceTag from "@/components/PriceTag";
 import { prisma } from "@/lib/db/prisma";
+import { Metadata } from "next";
 import Image from "next/image";
 import { notFound } from "next/navigation";
+import { cache } from "react";
 
 interface ProductPageProps {
   params: {
     id: string;
   };
 }
+const getProduct = cache(async (id: string) => {
+  const product = await prisma.product.findUnique({ where: { id } });
+  if (!product) notFound();
+  return product;
+});
+
+export async function generateMetadata({
+  params: { id },
+}: ProductPageProps): Promise<Metadata> {
+  const product = await getProduct(id);
+
+  return {
+    title: product.name + " - Alledrogo",
+    description: product.description,
+    openGraph: {
+      images: [{ url: product.imageUrl }],
+    },
+  };
+}
 
 export default async function ProductPage({
   params: { id },
 }: ProductPageProps) {
-  const product = await prisma.product.findUnique({ where: { id } });
-  if (!product) notFound();
+  const product = await getProduct(id);
   return (
-    <div className="flex flex-col gap-4 lg:flex-row">
+    <div className="flex flex-col gap-4 lg:flex-row lg:items-center">
       <Image
         src={product.imageUrl}
         alt={product.name}
