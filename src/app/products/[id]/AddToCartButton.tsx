973f047d23f6e898709fce224b6a2dc152f8389f
krzysztoffila <umowy.fila@gmail.com>
2023-10-26 15:44:47 +0200

    add server action in client component(add to cart)

--- src/app/products/[id]/AddToCartButton.tsx
+++ src/app/products/[id]/AddToCartButton.tsx
@@ -1,12 +1,30 @@
 "use client";
 
+import { useState, useTransition } from "react";
+
 interface AddToCartButtonProps {
   productId: string;
+  incrementProductQuantity: (productId: string) => Promise<void>;
 }
-export default function AddToCartButton({ productId }: AddToCartButtonProps) {
+export default function AddToCartButton({
+  productId,
+  incrementProductQuantity,
+}: AddToCartButtonProps) {
+  const [isPending, startTransition] = useTransition();
+  const [success, setSuccess] = useState(false);
+
   return (
     <div className="flex items-center gap-2">
-      <button className="btn-primary btn" onClick={() => {}}>
+      <button
+        className="btn-primary btn"
+        onClick={() => {
+          setSuccess(false);
+          startTransition(async () => {
+            await incrementProductQuantity(productId);
+            setSuccess(true);
+          });
+        }}
+      >
         Add to Cart
         <svg
           xmlns="http://www.w3.org/2000/svg"
@@ -23,6 +41,10 @@ export default function AddToCartButton({ productId }: AddToCartButtonProps) {
           />
         </svg>
       </button>
+      {isPending && <span className="loading loading-spinner loading-md" />}
+      {!isPending && success && (
+        <span className="text-success">Added to Cart.</span>
+      )}
     </div>
   );
 }
