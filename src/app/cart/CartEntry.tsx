64d8f90c3d5180e153409393837630e95f9a4496
krzysztoffila <umowy.fila@gmail.com>
2023-10-28 16:38:31 +0200

    create actions in shopping cart

--- src/app/cart/CartEntry.tsx
+++ src/app/cart/CartEntry.tsx
@@ -1,13 +1,25 @@
 "use client";
 
 import { CartItemWithProduct } from "@/lib/db/cart";
+import { formatPrice } from "@/lib/format";
 import Image from "next/image";
+import Link from "next/link";
 
 interface CartEntryProps {
     cartItem: CartItemWithProduct,
 }
 
 export default function CartEntry({ cartItem: { product, quantity } }: CartEntryProps) {
+
+    const quantityOptions: JSX.Element[] = [];
+    for (let i = 1; i <= 10; i++) {
+        quantityOptions.push(
+            <option value={i} key={i}>
+                {i}
+            </option>
+        )
+    }
+
     return (
         <div>
             <div className="felx flex-wrap items-center gap-3">
@@ -18,6 +30,29 @@ export default function CartEntry({ cartItem: { product, quantity } }: CartEntry
                     height={200}
                     className="rounded-lg"
                 />
+                <div>
+                    <Link href={"/products/" + product.id} className="font-bold">
+                        {product.name}
+                    </Link>
+                    <div>
+                        Price: {formatPrice(product.price)}
+                    </div>
+                    <div className="my-1 flex items-center gap-2">
+                        Quantity:
+                        <select
+                            className="select select-bordered w-full max-w-[80px]"
+                            defaultValue={quantity}
+                            onChange={e => {
+
+                            }}
+                        >
+                            {quantityOptions}
+                        </select>
+                    </div>
+                    <div className="flex items-center gap-3">
+                        Total: {formatPrice(product.price * quantity)}
+                    </div>
+                </div>
             </div>
             <div className="divider" />
         </div>
--- src/lib/format.ts
+++ src/lib/format.ts
@@ -1,5 +1,5 @@
 export function formatPrice(price: number) {
-  return (price / 100).toLocaleString("en-US", {
+  return (price / 10).toLocaleString("en-US", {
     style: "currency",
     currency: "USD",
   });
